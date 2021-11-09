1c9896c8808fcff9915b032f5f27daa2222ee9ba
Russell Gordon <rgordon@lcs.on.ca>
2021-11-09 06:20:47 -0500

    Initial Commit

---
+++ RectanglesAndSpacers/RectanglesAndSpacersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectanglesAndSpacersApp.swift
+//  RectanglesAndSpacers
+//
+//  Created by Russell Gordon on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct RectanglesAndSpacersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
