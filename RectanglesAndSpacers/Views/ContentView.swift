a54bf5df7e85d565fef3f99f926f32242c6b92b1
Russell Gordon <rgordon@lcs.on.ca>
2021-11-09 06:48:08 -0500

    Reproduced part of the volleyball card layout.

---
+++ RectanglesAndSpacers/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  RectanglesAndSpacers
+//
+//  Created by Russell Gordon on 2021-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+            // Layer 1
+            Color.white
+            
+            // Layer 2
+            Color.red
+                .padding()
+            
+            // Layer 3
+            HStack {
+                                
+                Color.white
+                    .frame(width: 15)
+                    .padding(16)
+                    .padding(.leading, 20)
+                
+                Spacer()
+            }
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
